18e28b4ee34606685f0ced730e7af138168fd686
madmuv <d.kritaisong@gmail.com>
2022-08-24 20:44:25 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:custom_clock/utils/time_model.dart';
+import 'package:custom_clock/widgets/clock_widget.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: Column(
+            children: [
+              ClockWidget(
+                TimeModel(
+                  12, 38, 10
+                ),
+              ),
+            ],
+        ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/clock_widget.dart
@@ -0,0 +1,104 @@
+import 'dart:math';
+
+import 'package:custom_clock/ui/style.dart';
+import 'package:flutter/material.dart';
+
+import '../utils/time_model.dart';
+
+class ClockWidget extends StatefulWidget {
+  ClockWidget(this.time, {Key? key}) : super(key: key);
+  TimeModel time;
+
+  @override
+  State<ClockWidget> createState() => _ClockWidgetState();
+}
+
+class _ClockWidgetState extends State<ClockWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(shape: BoxShape.circle),
+      height: 300.0,
+      width: 300.0,
+      child: CustomPaint(
+        painter: ClockPainter(widget.time),
+      ),
+    );
+  }
+}
+
+class ClockPainter extends CustomPainter {
+  TimeModel? time;
+
+  ClockPainter(this.time);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    // calculate time position
+    double secRad = ((pi / 2) - (pi / 30)) * time!.sec! % (2 * pi);
+    double minRad = ((pi / 2) - (pi / 30)) * time!.min! % (2 * pi);
+    double hourRad = ((pi / 2) - (pi / 9)) * time!.hour! % (2 * pi);
+
+    var centerX = size.width / 2;
+    var centerY = size.height / 2;
+    var center = Offset(centerX, centerY);
+    var radius = min(centerX, centerY);
+
+    // setting the clock coordinates
+    var secHeight = radius / 2;
+    var minHeight = radius / 2 - 10;
+    var hourHeight = radius / 2 - 25;
+
+    var seconds = Offset(
+        centerX + secHeight * cos(secRad), centerY - secHeight * sin(secRad));
+    var minutes = Offset(
+        centerX + minHeight * cos(minRad), centerY - minHeight * sin(minRad));
+    var hours = Offset(centerX + hourHeight * cos(hourRad),
+        centerY - hourHeight * sin(hourRad));
+
+    // setting the brush
+    var fillBrush = Paint()
+      ..color = AppStyle.primaryColor
+      ..strokeCap = StrokeCap.round;
+
+    var centerDotBrush = Paint()..color = const Color(0xFFEAECFF);
+
+    // setting the hand brush
+    var secBrush = Paint()
+      ..color = Colors.red
+      ..style = PaintingStyle.stroke
+      ..strokeCap = StrokeCap.round
+      ..strokeWidth = 2
+      ..strokeJoin = StrokeJoin.round;
+
+    var minBrush = Paint()
+      ..color = Colors.white
+      ..style = PaintingStyle.stroke
+      ..strokeCap = StrokeCap.round
+      ..strokeWidth = 3
+      ..strokeJoin = StrokeJoin.round;
+
+    var hourBrush = Paint()
+      ..color = Colors.white
+      ..style = PaintingStyle.stroke
+      ..strokeCap = StrokeCap.round
+      ..strokeWidth = 4
+      ..strokeJoin = StrokeJoin.round;
+
+    // draw the body
+    canvas.drawCircle(center, radius - 40, fillBrush);
+
+    // draw the clock hands
+    canvas.drawLine(center, seconds, secBrush);
+    canvas.drawLine(center, minutes, minBrush);
+    canvas.drawLine(center, hours, hourBrush);
+
+    // draw the center dot
+    canvas.drawCircle(center, 16, centerDotBrush);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+}
